4b87e47f0e20bac33949581ea80c7af0dd19961f
Valera <valery.muhanin@yandex.ru>
2023-03-14 14:09:56 +0300

    HoumeWork3

---
+++ src/Main.java
@@ -0,0 +1,117 @@
+public class Main {
+    public static void main(String[] args) {
+     int a = 40000;
+     short h = 10000;
+     byte b = 22;
+     long n = 90000;
+     float c = 3.75f;
+     float y = (int) (a + h + b + n + c);
+     System.out.println(y);
+
+     double sugar = 27.12;
+     long money = 9_876_789_655_539L;
+     float aplles = (float) 2.786F;
+     short road = 569;
+     short temperature = -159;
+     short kilogram = 27897;
+     byte weight = 67;
+     var H = (int) (sugar + money + aplles + road + temperature + kilogram + weight);
+     System.out.println(H);
+
+     byte LyudmilaPavlovna = 23;
+     byte AnnaSergeyevna = 27;
+     byte EkaterinaAndreevna = 30;
+     int TotalStudents = LyudmilaPavlovna + AnnaSergeyevna + EkaterinaAndreevna;
+     System.out.println(TotalStudents);
+     int TotalBoughtShrrts = 480;
+     byte Students = 80;
+     int SheetsPerStudent = TotalBoughtShrrts/Students;
+     System.out.println("Листов на ученика" + SheetsPerStudent + "Листы");
+
+     byte bottles = 16;
+     byte TimeBottle = 2;
+     int TwentyMinutes =160;
+     System.out.println("Произведенно бутылок за 20 минут" + TwentyMinutes);
+     int perDay = 11520;
+     System.out.println("Произведенно бутылок за день" + perDay);
+     int ThreeDays = 34560;
+     System.out.println("Произведенно бутылок за три дня" + ThreeDays);
+     int month = 345600;
+     System.out.println("Произведенно бутылок за месяц" + month);
+
+
+     byte totalDye = 120;
+     byte whiteDye = 2;
+     byte brownDye = 4;
+     byte OneClass = (byte) (whiteDye + brownDye);
+     System.out.println("Колличество баннок краски на один класс" + OneClass );
+     byte alllDye = 120;
+     byte dyeOneClass = 6;
+     byte classSchoool = (byte) (alllDye/dyeOneClass);
+     System.out.println("Колличество классов" + classSchoool);
+     byte Class = 20;
+     byte White = 2;
+     byte whiteDyeClass =(byte) (Class * White);
+     System.out.println("Колличество Белой краски" + whiteDyeClass);
+     byte brown = 4;
+     byte classes = 20;
+     byte brownDyeClass = (byte) (brown * classes);
+     System.out.println("Колличество Коричневой краски" + brownDyeClass);
+     byte boughtWhite = 40;
+     byte boughtBrown = 80;
+
+
+     int Bananas = 400;
+     int milkMll = 210;
+     int iceCream = 200;
+     int rawEggs = 280;
+     int weightKillogravs = (Bananas + milkMll + iceCream + rawEggs);
+     System.out.println(Bananas + milkMll + iceCream + rawEggs + "Вес в килограммах" + weightKillogravs);
+
+
+
+     int oneBananGramm = 400;
+     int milkGramm = 210;
+     int iceCreamGramm = 200;
+     int rawEggsGramm = 280;
+     System.out.println( oneBananGramm + milkGramm + iceCreamGramm + rawEggsGramm + "Вес продуктов в граммах" );
+
+
+     double WeightKg = 7.5;
+     int throwOffWeight = 250;
+     int i = (int) (WeightKg / throwOffWeight);
+     System.out.println(weightKillogravs/throwOffWeight + "Колличество дней нужное спортсмену");
+     double Kg = 7.5;
+     int OffWeight = 500;
+     int throwOff = (int) (Kg/OffWeight);
+     System.out.println(Kg/OffWeight + "Колличество дней нужное спортсмену");
+
+
+
+     int Masha = 67760;
+     int Denis = 83690;
+     int Kristina = 76230;
+     int employeeSalariesAre = 100;
+     int raiseEmployees = 10;
+     int i1 = Masha * raiseEmployees / employeeSalariesAre;
+     System.out.println(Masha * raiseEmployees / employeeSalariesAre + "Повышение Машеной зарплаты в днежном эквиваленте");
+     int i2 = Denis * raiseEmployees / employeeSalariesAre;
+     System.out.println(Denis * raiseEmployees / employeeSalariesAre + "Повышение ЗП Дениса в денижном эквиваленте");
+     int i3 = Kristina * raiseEmployees / employeeSalariesAre;
+     System.out.println(Kristina * raiseEmployees / employeeSalariesAre + "Повышение ЗП Кристины в денежном эквиваленте");
+     int MashaMoreOn = 6776;
+     System.out.println("Маша стала получать бльше на" + MashaMoreOn + "Руб");
+     int DenisMoreOn = 8369;
+     System.out.println("Денис стал получать больше на" + DenisMoreOn + "Руб");
+     int KreistinaMoreOn = 7623;
+     System.out.println("Крестина стала получать больше на" + KreistinaMoreOn + "Руб");
+
+
+
+
+
+    }
+
+
+    }
+
